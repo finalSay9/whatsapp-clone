8f590bf5e33bf4324768787ae3e28befc69a30ad
finalSay9 <chimwazaevan1@gmail.com>
2026-05-10 20:57:10 +0200

    creating the imports in the main.ts in gateway

--- apps/gateway/src/main.ts
+++ apps/gateway/src/main.ts
@@ -1,5 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { GatewayModule } from './gateway.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(GatewayModule);

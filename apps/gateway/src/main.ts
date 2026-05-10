cde5e03742ce8a25f4f5e23c31ebb56cfb54de22
finalSay9 <chimwazaevan1@gmail.com>
2026-05-10 21:13:16 +0200

    creating the validation pipe in main.ts gateway

--- apps/gateway/src/main.ts
+++ apps/gateway/src/main.ts
@@ -8,29 +8,30 @@ async function bootstrap() {
   const app = await NestFactory.create(GatewayModule);
 
   //validation pipe
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist: true,
-    forbidNonWhitelisted: true,
-    transform: true,
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
 
-  }));
-  
   // Swagger
   const config = new DocumentBuilder()
-    .setTitle('WhatsApp Clone API')
-    .setDescription('API Gateway — all requests go through here')
-    .setVersion('1.0')
+    .setTitle("WhatsApp Clone API")
+    .setDescription("API Gateway — all requests go through here")
+    .setVersion("1.0")
     .addBearerAuth(
-      { type: 'http', scheme: 'bearer', bearerFormat: 'JWT' },
-      'access-token',
+      { type: "http", scheme: "bearer", bearerFormat: "JWT" },
+      "access-token",
     )
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api/docs', app, document);
+  SwaggerModule.setup("api/docs", app, document);
 
   await app.listen(3000);
-  console.log('Gateway running on http://localhost:3000');
-  console.log('Swagger docs at http://localhost:3000/api/docs');
+  console.log("Gateway running on http://localhost:3000");
+  console.log("Swagger docs at http://localhost:3000/api/docs");
 }
 bootstrap();

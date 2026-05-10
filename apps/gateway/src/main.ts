d8418b5640bb4e016055a8d4f47bf50fead623f9
finalSay9 <chimwazaevan1@gmail.com>
2026-05-10 21:02:28 +0200

    creating the validation pipe in main.ts gateway

--- apps/gateway/src/main.ts
+++ apps/gateway/src/main.ts
@@ -6,6 +6,31 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(GatewayModule);
-  await app.listen(process.env.port ?? 3000);
+
+  //validation pipe
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    forbidNonWhitelisted: true,
+    transform: true,
+
+  }));
+  
+  // Swagger
+  const config = new DocumentBuilder()
+    .setTitle('WhatsApp Clone API')
+    .setDescription('API Gateway — all requests go through here')
+    .setVersion('1.0')
+    .addBearerAuth(
+      { type: 'http', scheme: 'bearer', bearerFormat: 'JWT' },
+      'access-token',
+    )
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
+  await app.listen(3000);
+  console.log('Gateway running on http://localhost:3000');
+  console.log('Swagger docs at http://localhost:3000/api/docs');
 }
 bootstrap();

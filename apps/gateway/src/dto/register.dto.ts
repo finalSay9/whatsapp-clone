76a551ca00f54fdeee116dea7c7bc0e69e2ff9d3
finalSay9 <chimwazaevan1@gmail.com>
2026-05-11 19:36:47 +0200

    creating the dto in the api gateway

---
+++ apps/gateway/src/dto/register.dto.ts
@@ -0,0 +1,18 @@
+// apps/gateway/src/dto/register.dto.ts
+import { IsEmail, IsString, MinLength } from "class-validator";
+import { ApiProperty } from "@nestjs/swagger";
+
+export class RegisterDto {
+  @ApiProperty({ example: "John Doe" })
+  @IsString()
+  name: string;
+
+  @ApiProperty({ example: "john@example.com" })
+  @IsEmail()
+  email: string;
+
+  @ApiProperty({ example: "password123" })
+  @IsString()
+  @MinLength(8)
+  password: string;
+}

153e33ce4a3f64aafe5193f033c823ec3f2214df
finalSay9 <chimwazaevan1@gmail.com>
2026-05-12 10:48:29 +0200

    implementing the canActivate method in guards

--- apps/gateway/guards/guard.jwt.ts
+++ apps/gateway/guards/guard.jwt.ts
@@ -6,10 +6,16 @@ import {
     Inject
 } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
-import { firstValueFrom } from 'rxjs';
+import { firstValueFrom, Observable } from 'rxjs';
 import { Request } from 'express';
 
 @Injectable()
 export class JwtGuard implements CanActivate {
-    
+    constructor(@Inject('AUTH_SERVICE') private readonly authClient: ClientProxy){}
+    canActivate(
+        context: ExecutionContext): Promise<boolean> {
+        
+    }
+
+
 }

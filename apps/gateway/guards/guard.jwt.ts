1a6df2966cc22d950d4f8782feac50e0a4e012f8
finalSay9 <chimwazaevan1@gmail.com>
2026-05-12 10:23:26 +0200

    exporting the class that implemets canActivate

--- apps/gateway/guards/guard.jwt.ts
+++ apps/gateway/guards/guard.jwt.ts
@@ -6,3 +6,10 @@ import {
     Inject
 } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
+import { firstValueFrom } from 'rxjs';
+import { Request } from 'express';
+
+@Injectable()
+export class JwtGuard implements CanActivate {
+    
+}

d050a3db4389087d36c2c83313b5942f90423863
finalSay9 <chimwazaevan1@gmail.com>
2026-05-12 10:55:22 +0200

    calling the helper function to extract the token

--- apps/gateway/guards/guard.jwt.ts
+++ apps/gateway/guards/guard.jwt.ts
@@ -12,9 +12,15 @@ import { Request } from 'express';
 @Injectable()
 export class JwtGuard implements CanActivate {
     constructor(@Inject('AUTH_SERVICE') private readonly authClient: ClientProxy){}
-    canActivate(
-        context: ExecutionContext): Promise<boolean> {
+    canActivate(context: ExecutionContext): Promise<boolean> {
+        const request = context.switchToHttp().getRequest<Request>()
         
+        //extract token from the authorization header
+        const token = this.extractToken(request)
+        //if token not provided
+        if(!token) {
+            throw new UnauthorizedException('No Token Provide')
+        }
     }
 
 

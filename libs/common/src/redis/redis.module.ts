6fea107b07442decb522289701a5279e5c8cb615
finalSay9 <chimwazaevan1@gmail.com>
2026-05-13 15:06:30 +0200

    finalizing the redis class module

--- libs/common/src/redis/redis.module.ts
+++ libs/common/src/redis/redis.module.ts
@@ -1,20 +1,37 @@
-import { Global, Module } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
-import Redis from 'ioredis';
+// libs/common/src/redis/redis.module.ts
+import { Global, Module } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import Redis from "ioredis";
 
-
-export const REDIS_CLIENT = 'REDIS_CLIENT';
-export const REDIS_SUBSCRIBER = 'REDIS_SUBSCRIBER';
+export const REDIS_CLIENT = "REDIS_CLIENT";
+export const REDIS_SUBSCRIBER = "REDIS_SUBSCRIBER";
 
 @Global()
 @Module({
-    providers: [{
-        provide: REDIS_CLIENT,
-        useFactory: (config: ConfigService) => {
-            return new Redis({
-                host: config.get('REDIS_HOST', 'localhost'),
-                port: config.get<number>('REDIS_PORT', 6379)
-            });
-        }
-    }]
+  providers: [
+    {
+      provide: REDIS_CLIENT,
+      useFactory: (config: ConfigService) => {
+        return new Redis({
+          host: config.get("REDIS_HOST", "localhost"),
+          port: config.get<number>("REDIS_PORT", 6379),
+        });
+      },
+      inject: [ConfigService],
+    },
+    {
+      provide: REDIS_SUBSCRIBER,
+      useFactory: (config: ConfigService) => {
+        // separate connection for subscribing
+        // Redis requires a dedicated connection for pub/sub
+        return new Redis({
+          host: config.get("REDIS_HOST", "localhost"),
+          port: config.get<number>("REDIS_PORT", 6379),
+        });
+      },
+      inject: [ConfigService],
+    },
+  ],
+  exports: [REDIS_CLIENT, REDIS_SUBSCRIBER],
 })
+export class RedisModule {}

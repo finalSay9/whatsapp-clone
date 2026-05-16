af2dca673e3e1d21e8254d6af74c6641c62c0a9b
finalSay9 <chimwazaevan1@gmail.com>
2026-05-16 15:33:46 +0200

    making some changes andinstalling some needed dependencies

--- apps/messages/src/main.ts
+++ apps/messages/src/main.ts
@@ -1,8 +1,21 @@
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { NestFactory } from '@nestjs/core';
 import { MessagesModule } from './messages.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(MessagesModule);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    MessagesModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        host: '0.0.0.0',
+        port: 3009
+      }
+    }
+  );
+
+
+
   await app.listen(process.env.port ?? 3000);
 }
 bootstrap();

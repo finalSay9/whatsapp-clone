f78eb4d303930a5aade6cb52c5848431ba3c8cd9
finalSay9 <chimwazaevan1@gmail.com>
2026-05-16 20:04:32 +0200

    importing the config module and his friend config service

--- apps/messages/src/messages.module.ts
+++ apps/messages/src/messages.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { MessagesController } from './messages.controller';
 import { MessagesService } from './messages.service';
+import { ConfigModule, ConfigService } from "@nestjs/config";
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      
+    })
+  ],
   controllers: [MessagesController],
   providers: [MessagesService],
 })

4fa9514acd44b98893ba1deee9b4c3e7f3203d07
finalSay9 <chimwazaevan1@gmail.com>
2026-05-20 12:52:25 +0200

    creating the dto in the chat directory to validate the messages send/received

--- apps/gateway/chat/dto/send-message.dto.ts
+++ apps/gateway/chat/dto/send-message.dto.ts
@@ -1,6 +1,13 @@
-import { IsString, IsUUID, IsNotEmpty, MaxLenght } from 'class-validator';
+import { IsString, IsUUID, IsNotEmpty, MaxLength } from 'class-validator';
 
 
 export class SendMessageDto{
-    
+    @IsUUID()
+    recipientId: string
+
+    @IsString()
+    @IsNotEmpty()
+    @MaxLength(1000)
+    content: string
+
 }
